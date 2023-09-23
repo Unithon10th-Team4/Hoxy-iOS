b218327d8a95b25a498effa48345d99f6b50fec7
Daegun Choi  <chcs1370@gmail.com>
2023-09-23 10:14:30 +0900

    add: 기본 프로젝트 생성

---
+++ hoxy/hoxy/hoxyApp.swift
@@ -0,0 +1,17 @@
+//
+//  hoxyApp.swift
+//  hoxy
+//
+//  Created by Daegeon Choi on 2023/09/23.
+//
+
+import SwiftUI
+
+@main
+struct hoxyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
