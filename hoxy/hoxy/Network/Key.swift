f103562a4aa531573182fb6aa1790399e39afb0c
Daegun Choi  <chcs1370@gmail.com>
2023-10-05 19:09:10 +0900

    Key.swift 파일 업로드

---
+++ hoxy/hoxy/Network/Key.swift
@@ -0,0 +1,11 @@
+//
+//  Key.swift
+//  hoxy
+//
+//  Created by Daegeon Choi on 2023/09/24.
+//
+
+import Foundation
+
+// BaseUrl not working due to Server Shut down
+let baseUrl = "https://first-alien-alert.ngrok-free.app/"
